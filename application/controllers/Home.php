ef79d82a4ad6b34a00a325d02871317df1f99b37
Raymar Monte <raymarmonte@gmail.com>
2015-09-13 14:47:41 +0800

    Populated movie table

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,12 +1,17 @@
 <?php
 class Home extends CI_Controller {
+    
+    public function __construct(){
+      parent::__construct();
+      $this->load->helper('url');     
+    }   
+
 	public function home(){
-		$this->load->helper('url');
 		$data['title'] = 'SineReserve';
+		$data['posters'] = null;
 		$this->load->view('index', $data);
 	}
 	public function movie(){
-		$this->load->helper('url');
 		$data['title'] = 'Movie Page';
 		$this->load->view('moviepage', $data);
 	}
