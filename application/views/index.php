c7544868157a93d90f5d770925a5b582397aaeb6
WalterJann <walterjannpura@yahoo.com>
2015-10-15 02:02:03 +0800

    index.php

--- application/views/index.php
+++ application/views/index.php
@@ -15,7 +15,8 @@
   		<link href="<?php echo base_url();?>assets/css/slick.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   		<link href="<?php echo base_url();?>assets/css/slick-theme.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   		<link href="<?php echo base_url();?>assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		
+  		<link href="<?php echo base_url();?>assets/css/sticky.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
   		<link href="<?php echo base_url();?>assets/css/lol.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	</head>
@@ -192,6 +193,31 @@
       		?>
       	</div>
 	    </div>
+	<div>
+	<footer class="page-footer red darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">SineReserve</h5>
+		<p class="white-text">A website where you can view currently showing and coming soon movies and reserve tickets.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul class="list">
+                  <li><a class="white-text" href="http://localhost/SineReserve/index.php/Home/home">Home</a></li>
+                  <li><a class="white-text" href="http://localhost/SineReserve/index.php/movie_page_controller/movie">Movies</a></li>
+                  <li><a class="white-text" href="http://localhost/SineReserve/index.php/Admin_controller/index">Admin</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            <p class="white-text">© 2015 All Rights Reserved</p>
+            </div>
+          </div>
+        </footer>
+	</div>
 
 	    <!-- Modals -->
       
