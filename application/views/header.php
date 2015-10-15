aac7668f52cfa8f5c84fe4ce5bc3ec4a8194fd0d
Dan Falcon <dan_1196@yahoo.com>
2015-10-15 17:19:33 +0800

    Ok on header division. Fixed home issues.

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,29 @@
+<!DOCTYPE html>
 <html>
+
   <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+      
+      <title>Home Page</title>
+
+      <!-- CSS  -->
+      <link href="<?php echo base_url();?>assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/new-moviebox-style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/gridlist.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/expand.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/slick.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/slick-theme.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="<?php echo base_url();?>assets/css/sticky.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+
+      <link href="<?php echo base_url();?>assets/css/lol.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   </head>
-  <body>
+  
+  <body class="lighten-5" style="background-color: #18191b;">
+
+    <!-- Navigation Bar -->
     <div class ="navbar">
       <nav class="red darken-4 main-nav ripple" id="meet-1" role="navigation">
         <div class="nav-wrapper container"> 
--- application/views/index.php
+++ application/views/index.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-  		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  		
-  		<title>Home Page</title>
-
-  		<!-- CSS  -->
-  		<link href="<?php echo base_url();?>assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/new-moviebox-style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/gridlist.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/expand.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/slick.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/slick-theme.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="<?php echo base_url();?>assets/css/sticky.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-
-  		<link href="<?php echo base_url();?>assets/css/lol.css" type="text/css" rel="stylesheet" media="screen,projection"/>
-  		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	</head>
-	
-	<body class="lighten-5" style="background-color: #18191b;">
-
-		<!-- Navigation Bar -->
 
 		<!-- Banner -->
 		<div class="row banner-with-bg"
