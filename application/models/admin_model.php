3d891e897eb8bcb0295154042544739911fbe5aa
Michelle Ann Mendoza <mendozamichelleann07@yahoo.com>
2015-10-17 13:09:01 +0800

    Admin Page Iterations

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -11,5 +11,42 @@
 			if($query->num_rows() > 0)
 				return $query->result();
 		}
+		
+		public function set_branch($name, $address){
+			if(($name != null) && ($address != null)){
+				$branch = array(
+					'bran_name' => $name,
+					'bran_address' => $address,
+				);
+				$this->db->insert('branch', $branch);
+			}
+		}
+		
+		public function get_cinema_in_branch($branch = null){
+			$where = array('cinema.bran_id' => $branch);
+			$this->db->select();
+			$this->db->from('cinema');
+			$this->db->join('branch', 'branch.bran_id = cinema.bran_id');
+			$this->db->where($where);
+			$query = $this->db->get();
+			if($query->num_rows() > 0){
+				$res = array('result' => $query->result(), 'last_row' => $query->last_row());
+				return $res;
+			} 
+			else
+				return null;
+		} 
+		
+		public function insert_cinema_seat($cine_name, $bran_id, $cine_slots){
+			if(($cine_name != null) && ($bran_id != null) && ($cine_slots != null)){
+				$seat = array(
+					'cine_name' => $cine_name,
+					'bran_id' => $bran_id,
+					'cine_slots' => $cine_slots,
+				);
+				$this->db->insert('cinema', $seat);
+			}
+			
+		}		
 	}
 ?>
---
+++ application/views/Admin_Navigation.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<!-- META TAGS -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title><?php echo $title_page?></title>
+		
+		<!-- CSS TAGS -->
+		<link href="<?php echo base_url();?>assets/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+		
+		<style>
+			main {
+				padding-left: 260px;
+				padding-right: 10%;
+			}
+			
+			.side_nav{
+				top: inherit;
+			}
+			
+			.modal{
+				max-height: 100%;
+			}
+			
+			.branch_dropdown {
+				padding-top: 30px;
+			}
+			
+			#plot_field{
+				overflow-y: auto;
+				max-height: 10px;
+			}
+			
+			#textarea_plot{
+				max-height: 100px;
+			}
+			
+			@media only screen and (max-width : 992px) {
+				main {
+					padding-left: 20px;
+				}
+			}
+		</style>
+	</head>
+	<body>
+		<div class="navbar-fixed">
+			<nav>
+				<div class="nav-wrapper container">
+					<a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+					<a href="#!" class="brand-logo">Sinereserve</a>
+					<ul class="right hide-on-med-and-down">
+						<li><a href="sass.html">Log Out</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<ul id="slide-out" class="side-nav fixed side_nav">
+			<li><a href="<?php echo base_url();?>index.php/Admin_controller/branch">Branches</a></li>
+			<li><a href="<?php echo base_url();?>index.php/Admin_controller/cinema">Cinema</a></li>
+			<li><a href="<?php echo base_url();?>index.php/Admin_controller/movie">Movies</a></li>
+			<li><a href="#!">Shows</a></li>
+		</ul>
